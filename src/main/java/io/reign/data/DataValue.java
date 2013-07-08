ea4383f389ac1f93834ae31bd71351fd81eddc35
ypai <ypai@kompany.org>
2013-07-07 22:53:40 -0700

    changed default index

--- src/main/java/io/reign/data/DataValue.java
+++ src/main/java/io/reign/data/DataValue.java
@@ -12,7 +12,7 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
-*/
+ */
 
 package io.reign.data;
 
@@ -23,7 +23,7 @@ package io.reign.data;
  */
 public interface DataValue<T> {
 
-    public static final String DEFAULT_INDEX = "0";
+    public static final String DEFAULT_INDEX = "_";
 
     /**
      * For a data node that has multiple values, this unique index is used to distinguish one value from another in
