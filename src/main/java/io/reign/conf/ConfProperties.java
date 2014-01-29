4fc88358ae82fcae5e987341bd5bc38cb48b94e4
Yen Pai <yen@tubemogul.com>
2014-01-29 11:05:47 -0800

    comments

--- src/main/java/io/reign/conf/ConfProperties.java
+++ src/main/java/io/reign/conf/ConfProperties.java
@@ -12,7 +12,7 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
-*/
+ */
 
 package io.reign.conf;
 
@@ -58,7 +58,6 @@ public class ConfProperties extends Properties {
 
     @Override
     public synchronized int hashCode() {
-        // TODO Auto-generated method stub
         return super.hashCode();
     }
 
