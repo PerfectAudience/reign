64037fdf354b8ae9c06d52f1d74c1335fbed64ca
Yen Pai <ypai@kompany.org>
2014-04-01 21:22:15 -0700

    took out service notification on node creation

--- src/main/java/io/reign/presence/PresenceObserver.java
+++ src/main/java/io/reign/presence/PresenceObserver.java
@@ -95,11 +95,7 @@ public abstract class PresenceObserver<T> extends AbstractObserver {
             updated((T) updated, null);
             return;
         }
-        if (serviceId != null) {
-            ServiceInfo updated = new ServiceInfo(clusterId, serviceId, childList);
-            updated((T) updated, null);
-            return;
-        }
+
     }
 
 }
