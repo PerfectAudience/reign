949d5d3f7f7c7893efca2c6cd4eaae85bf2efb05
Yen Pai <ypai@kompany.org>
2014-04-21 23:22:12 -0700

    added initial equals() and hashCode() methods

--- src/main/java/io/reign/DefaultNodeId.java
+++ src/main/java/io/reign/DefaultNodeId.java
@@ -47,14 +47,6 @@ public class DefaultNodeId implements NodeId {
     @JsonProperty("h")
     private String host;
 
-    // /**
-    // * the port for an available service (short-cut to specifying it as an node attribute -- fewer ZK operations if
-    // this
-    // * is all that is needed to interact with a service once discovered)
-    // */
-    // @JsonProperty("p")
-    // private Integer port;
-
     /** the messaging port for the framework */
     @JsonProperty("mp")
     private Integer messagingPort;
@@ -71,65 +63,25 @@ public class DefaultNodeId implements NodeId {
         this.messagingPort = messagingPort;
     }
 
-    // public static DefaultCanonicalId id() {
-    // return new DefaultCanonicalId();
-    // }
-
-    // @Override
-    // public DefaultCanonicalId port(Integer port) {
-    // setPort(port);
-    // return this;
-    // }
-
     @Override
     public String getProcessId() {
         return processId;
     }
-
-    // @Override
-    // public void setProcessId(String processId) {
-    // this.processId = processId;
-    // }
 
     @Override
     public String getIpAddress() {
         return ipAddress;
     }
 
-    // @Override
-    // public void setIpAddress(String ipAddress) {
-    // this.ipAddress = ipAddress;
-    // }
-
     @Override
     public String getHost() {
         return host;
     }
 
-    // @Override
-    // public void setHost(String host) {
-    // this.host = host;
-    // }
-
-    // @Override
-    // public Integer getPort() {
-    // return port;
-    // }
-
-    // @Override
-    // public void setPort(Integer port) {
-    // this.port = port;
-    // }
-
     @Override
     public Integer getMessagingPort() {
         return messagingPort;
     }
-
-    // @Override
-    // public void setMessagingPort(Integer messagingPort) {
-    // this.messagingPort = messagingPort;
-    // }
 
     @Override
     public String toString() {
@@ -138,6 +90,20 @@ public class DefaultNodeId implements NodeId {
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
         }
+    }
+
+    @Override
+    public boolean equals(Object in) {
+        if (in == null || !(in instanceof DefaultNodeId)) {
+            return false;
+        }
+
+        return in.toString().equals(this.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
     }
 
 }
