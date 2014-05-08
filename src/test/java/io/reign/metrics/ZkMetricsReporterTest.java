557ffe733e450d43c7f3a40b8a6ed8e578d683e9
Yen Pai <ypai@kompany.org>
2014-05-08 14:02:42 -0700

    fixed json rendering issue

--- src/test/java/io/reign/metrics/ZkMetricsReporterTest.java
+++ src/test/java/io/reign/metrics/ZkMetricsReporterTest.java
@@ -169,4 +169,33 @@ public class ZkMetricsReporterTest {
             assertFalse(true);
         }
     }
+
+    @Test
+    public void testSingleCounterSingleTimer() throws Exception {
+        try {
+            StaticMetricRegistryManager registryManager = new StaticMetricRegistryManager();
+            Counter counter = registryManager.get().counter("testCounter");
+            counter.inc();
+            Timer timer = registryManager.get().timer("testTimer");
+            Context timerContext = timer.time();
+            timerContext.stop();
+
+            final ZkMetricsReporter reporter = ZkMetricsReporter.builder().convertRatesTo(TimeUnit.SECONDS)
+                    .convertDurationsTo(TimeUnit.MILLISECONDS).build();
+
+            String string = reporter.report(registryManager.get(), registryManager.getLastRotatedTimestamp(),
+                    registryManager.getRotationInterval(), registryManager.getRotationTimeUnit(), new StringBuilder())
+                    .toString();
+            logger.debug(string);
+
+            byte[] bytes = string.getBytes("UTF-8");
+            MetricsData metricsData = JacksonUtil.getObjectMapper().readValue(bytes, MetricsData.class);
+            assertTrue(metricsData.getCounters().size() == 1);
+            assertTrue(metricsData.getCounter("testCounter").getCount() == 1);
+            assertTrue(metricsData.getTimers().size() == 1);
+        } catch (Exception e) {
+            logger.error("" + e, e);
+            assertFalse(true);
+        }
+    }
 }
