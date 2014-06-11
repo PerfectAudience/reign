1313ae57d446881fd1f81bd6e202cc967ecf2cf7
Yen Pai <ypai@kompany.org>
2014-06-10 21:37:09 -0700

    altered check for path token

--- src/main/java/io/reign/DefaultPathScheme.java
+++ src/main/java/io/reign/DefaultPathScheme.java
@@ -1,17 +1,14 @@
 /*
- Copyright 2013 Yen Pai ypai@reign.io
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright 2013 Yen Pai ypai@reign.io
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package io.reign;
@@ -138,7 +135,8 @@ public class DefaultPathScheme implements PathScheme {
 
     @Override
     public boolean isValidToken(String pathToken) {
-        return !StringUtils.isBlank(pathToken) && pathToken.indexOf('/') == -1;
+        return !StringUtils.isBlank(pathToken) && pathToken.charAt(0) != '/'
+                && pathToken.charAt(pathToken.length() - 1) != '/';
     }
 
     @Override
--- src/test/java/io/reign/DefaultPathSchemeTest.java
+++ src/test/java/io/reign/DefaultPathSchemeTest.java
@@ -1,21 +1,19 @@
 /*
- Copyright 2013 Yen Pai ypai@reign.io
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright 2013 Yen Pai ypai@reign.io
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package io.reign;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -32,6 +30,19 @@ public class DefaultPathSchemeTest {
     @Before
     public void setUp() throws Exception {
         pathScheme = new DefaultPathScheme(Reign.DEFAULT_FRAMEWORK_BASE_PATH, Reign.DEFAULT_FRAMEWORK_CLUSTER_ID);
+    }
+
+    @Test
+    public void testIsValidPathToken() throws Exception {
+        assertTrue(pathScheme.isValidToken("jack"));
+        assertTrue(pathScheme.isValidToken("jack/jill"));
+        assertTrue(pathScheme.isValidToken("jack/jill/up/the"));
+        assertTrue(pathScheme.isValidToken("jack/jill/up/the/hill"));
+
+        assertFalse(pathScheme.isValidToken("/jack"));
+        assertFalse(pathScheme.isValidToken("jack/jill/"));
+        assertFalse(pathScheme.isValidToken("/jack/jill/up/the"));
+        assertFalse(pathScheme.isValidToken("jack/jill/up/the/hill/"));
     }
 
     @Test
