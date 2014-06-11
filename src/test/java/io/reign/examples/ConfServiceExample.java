e57678636c7c818b1f847df3c5cafc3d4f6f13e6
Yen Pai <ypai@kompany.org>
2014-06-10 21:36:13 -0700

    removed Properties support to simplify

--- src/test/java/io/reign/examples/ConfServiceExample.java
+++ src/test/java/io/reign/examples/ConfServiceExample.java
@@ -1,26 +1,23 @@
 /*
- Copyright 2013 Yen Pai ypai@reign.io
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright 2013 Yen Pai ypai@reign.io
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package io.reign.examples;
 
 import io.reign.Reign;
 import io.reign.conf.ConfObserver;
-import io.reign.conf.ConfProperties;
 import io.reign.conf.ConfService;
 
+import java.util.Map;
 import java.util.Properties;
 
 import org.slf4j.Logger;
@@ -86,9 +83,9 @@ public class ConfServiceExample {
         // load a configuration which will not be immediately available but pass
         // observer to be notified of changes in configuration
         Properties loadedConf = null;
-        confService.observe("examples", "config1.properties", new ConfObserver<ConfProperties>() {
+        confService.observe("examples", "config1.properties", new ConfObserver<Map<String, Object>>() {
             @Override
-            public void updated(ConfProperties conf, ConfProperties oldConf) {
+            public void updated(Map<String, Object> conf, Map<String, Object> oldConf) {
                 if (conf != null) {
                     logger.info("***** Observer:  conf={}; oldConf={}", conf, oldConf);
 
