ef2516c6766f5dd89def39ccac4b0c609904104c
Yen Pai <ypai@kompany.org>
2014-07-05 23:48:17 -0700

    unit test

---
+++ src/test/java/io/reign/presence/UpdatingServiceInfoTest.java
@@ -0,0 +1,76 @@
+package io.reign.presence;
+
+import static org.junit.Assert.assertTrue;
+import io.reign.MasterTestSuite;
+import io.reign.ObserverManager;
+import io.reign.PathScheme;
+import io.reign.PathType;
+import io.reign.ReignContext;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UpdatingServiceInfoTest {
+
+	private PresenceService presenceService;
+
+	@Before
+	public void setUp() throws Exception {
+		presenceService = MasterTestSuite.getReign().getService("presence");
+	}
+
+	@Test
+	public void testBasic() throws Exception {
+		UpdatingServiceInfo serviceInfo = null;
+		try {
+			ReignContext context = MasterTestSuite.getReign().getContext();
+			PathScheme pathScheme = context.getPathScheme();
+
+			// create self-updating Conf object, should be empty initially
+			serviceInfo = new UpdatingServiceInfo("clusterA", "serviceA",
+					context);
+			assertTrue("clusterA".equals(serviceInfo.getClusterId()));
+			assertTrue("serviceA".equals(serviceInfo.getServiceId()));
+			assertTrue(serviceInfo.getNodeIdList().size() == 0);
+
+			// edit configuration, change should be reflected in some period of
+			// time
+			presenceService.announce("clusterA", "serviceA", true);
+
+			// wait up to 30 seconds total for updates
+			for (int i = 0; i < 10; i++) {
+				Thread.sleep(3000);
+				if (serviceInfo.getNodeIdList().size() == 1) {
+					break;
+				}
+			}
+
+			assertTrue("clusterA".equals(serviceInfo.getClusterId()));
+			assertTrue("serviceA".equals(serviceInfo.getServiceId()));
+			assertTrue(serviceInfo.getNodeIdList().size() == 1);
+			assertTrue(serviceInfo.getNodeIdList().get(0)
+					.equals(pathScheme.toPathToken(context.getNodeId())));
+		} finally {
+			serviceInfo.destroy();
+		}
+	}
+
+	@Test
+	public void testDestroy() throws Exception {
+		UpdatingServiceInfo serviceInfo = null;
+
+		ReignContext context = MasterTestSuite.getReign().getContext();
+		ObserverManager observerManager = context.getObserverManager();
+		String path = context.getPathScheme().getAbsolutePath(
+				PathType.PRESENCE, "clusterA", "serviceB");
+
+		// create self-updating Conf object, observer should be registered
+		serviceInfo = new UpdatingServiceInfo("clusterA", "serviceB", context);
+		assertTrue(observerManager.getReadOnlyObserverSet(path).size() == 1);
+
+		// destroy object, observer should be removed
+		serviceInfo.destroy();
+		assertTrue(observerManager.getReadOnlyObserverSet(path).size() == 0);
+
+	}
+}
