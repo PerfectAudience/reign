3d7f8c5bfe35d8d48d0a58a306fd133547c738d0
Yen Pai <ypai@kompany.org>
2014-07-07 21:20:15 -0700

    handling initial null

--- src/main/java/io/reign/presence/UpdatingNodeInfo.java
+++ src/main/java/io/reign/presence/UpdatingNodeInfo.java
@@ -29,57 +29,75 @@ import java.util.Map;
  */
 public class UpdatingNodeInfo implements NodeInfo {
 
-    private volatile NodeInfo nodeInfo;
-    private final String clusterId;
-    private final String serviceId;
-    private NodeId nodeId;
-    private final ReignContext context;
-    private final PresenceObserver<NodeInfo> observer;
+	private volatile NodeInfo nodeInfo;
+	private final String clusterId;
+	private final String serviceId;
+	private NodeId nodeId;
+	private final ReignContext context;
+	private final PresenceObserver<NodeInfo> observer;
 
-    public UpdatingNodeInfo(String clusterId, String serviceId, NodeId nodeId, ReignContext context) {
-        if (clusterId == null && serviceId == null && nodeId == null) {
-            throw new IllegalArgumentException("clusterId, serviceId, nodeId cannot be null!");
-        }
+	public UpdatingNodeInfo(String clusterId, String serviceId, NodeId nodeId,
+			ReignContext context) {
+		if (clusterId == null && serviceId == null && nodeId == null) {
+			throw new IllegalArgumentException(
+					"clusterId, serviceId, nodeId cannot be null!");
+		}
 
-        this.clusterId = clusterId;
-        this.serviceId = serviceId;
-        this.nodeId = nodeId;
-        this.context = context;
-        this.observer = new PresenceObserver<NodeInfo>() {
-            @Override
-            public void updated(NodeInfo updated, NodeInfo previous) {
-                nodeInfo = updated;
-            }
-        };
+		this.clusterId = clusterId;
+		this.serviceId = serviceId;
+		this.nodeId = nodeId;
+		this.context = context;
+		this.observer = new PresenceObserver<NodeInfo>() {
+			@Override
+			public void updated(NodeInfo updated, NodeInfo previous) {
+				nodeInfo = updated;
+			}
+		};
 
-        PresenceService presenceService = context.getService("presence");
-        nodeInfo = presenceService.getNodeInfo(clusterId, serviceId, context.getPathScheme().toPathToken(nodeId),
-                observer);
-    }
+		PresenceService presenceService = context.getService("presence");
+		nodeInfo = presenceService.getNodeInfo(clusterId, serviceId, context
+				.getPathScheme().toPathToken(nodeId), observer);
+	}
 
-    public void destroy() {
-        String path = context.getPathScheme().getAbsolutePath(PathType.PRESENCE, clusterId, serviceId,
-                context.getPathScheme().toPathToken(nodeId));
-        context.getObserverManager().remove(path, observer);
-    }
+	public void destroy() {
+		String path = context.getPathScheme().getAbsolutePath(
+				PathType.PRESENCE, clusterId, serviceId,
+				context.getPathScheme().toPathToken(nodeId));
+		context.getObserverManager().remove(path, observer);
+	}
 
-    public Object getAttribute(String key) {
-        return nodeInfo.getAttribute(key);
-    }
+	public Object getAttribute(String key) {
+		if (nodeInfo == null) {
+			return null;
+		}
+		return nodeInfo.getAttribute(key);
+	}
 
-    public Map<String, String> getAttributeMap() {
-        return nodeInfo.getAttributeMap();
-    }
+	public Map<String, String> getAttributeMap() {
+		if (nodeInfo == null) {
+			return null;
+		}
+		return nodeInfo.getAttributeMap();
+	}
 
-    public String getClusterId() {
-        return nodeInfo.getClusterId();
-    }
+	public String getClusterId() {
+		if (nodeInfo == null) {
+			return null;
+		}
+		return nodeInfo.getClusterId();
+	}
 
-    public String getServiceId() {
-        return nodeInfo.getServiceId();
-    }
+	public String getServiceId() {
+		if (nodeInfo == null) {
+			return null;
+		}
+		return nodeInfo.getServiceId();
+	}
 
-    public NodeId getNodeId() {
-        return nodeInfo.getNodeId();
-    }
+	public NodeId getNodeId() {
+		if (nodeInfo == null) {
+			return null;
+		}
+		return nodeInfo.getNodeId();
+	}
 }
