fa721499235d6e4533e5a3638698bd711a6e1a8a
Yen Pai <ypai@kompany.org>
2014-07-09 23:15:43 -0700

    source code formatting

--- src/main/java/io/reign/data/KryoTranscodingScheme.java
+++ src/main/java/io/reign/data/KryoTranscodingScheme.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
 import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.KryoException;
 import com.esotericsoftware.kryo.Serializer;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
@@ -17,47 +16,49 @@ import com.esotericsoftware.kryo.io.Output;
  */
 public class KryoTranscodingScheme implements TranscodingScheme {
 
-    /**
-     * Kryo is not thread-safe.
-     */
-    private static final ThreadLocal<Kryo> kryoThreadLocal = new ThreadLocal() {
-        @Override
-        protected Kryo initialValue() {
-            Kryo kryo = new Kryo();
-            return kryo;
-        }
+	/**
+	 * Kryo is not thread-safe.
+	 */
+	private static final ThreadLocal<Kryo> KRYO_THREAD_LOCAL = new ThreadLocal<Kryo>() {
+		@Override
+		protected Kryo initialValue() {
+			Kryo kryo = new Kryo();
+			return kryo;
+		}
 
-    };
+	};
 
-    public KryoTranscodingScheme() {
-    }
+	public KryoTranscodingScheme() {
+	}
 
-    @Override
-    public byte[] toBytes(Object value) {
-        if (value == null)
-            return null;
+	@Override
+	public byte[] toBytes(Object value) {
+		if (value == null) {
+			return null;
+		}
 
-        Kryo kryo = kryoThreadLocal.get();
-        Output out = new Output(new ByteArrayOutputStream());
-        kryo.writeObject(out, value);
-        byte[] bytes = out.toBytes();
-        out.close();
-        return bytes;
-    }
+		Kryo kryo = KRYO_THREAD_LOCAL.get();
+		Output out = new Output(new ByteArrayOutputStream());
+		kryo.writeObject(out, value);
+		byte[] bytes = out.toBytes();
+		out.close();
+		return bytes;
+	}
 
-    @Override
-    public <T> T fromBytes(byte[] bytes, Class clazz) {
-        if (bytes == null || bytes.length == 0)
-            return null;
-        Kryo kryo = kryoThreadLocal.get();
-        Input input = new Input(new ByteArrayInputStream(bytes));
-        T value = (T) kryo.readObject(input, clazz);
-        input.close();
-        return value;
-    }
+	@Override
+	public <T> T fromBytes(byte[] bytes, Class clazz) {
+		if (bytes == null || bytes.length == 0) {
+			return null;
+		}
+		Kryo kryo = KRYO_THREAD_LOCAL.get();
+		Input input = new Input(new ByteArrayInputStream(bytes));
+		T value = (T) kryo.readObject(input, clazz);
+		input.close();
+		return value;
+	}
 
-    public void register(Class clazz, Serializer serializer) {
-        Kryo kryo = kryoThreadLocal.get();
-        kryo.register(clazz, serializer);
-    }
+	public void register(Class clazz, Serializer serializer) {
+		Kryo kryo = KRYO_THREAD_LOCAL.get();
+		kryo.register(clazz, serializer);
+	}
 }
