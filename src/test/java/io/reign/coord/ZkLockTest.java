9f3a299e6a4b25dff477c7531f6c7932bacddc24
Yen Pai <ypai@kompany.org>
2014-07-15 10:39:55 -0700

    more predictable, realistic test

--- src/test/java/io/reign/coord/ZkLockTest.java
+++ src/test/java/io/reign/coord/ZkLockTest.java
@@ -3,6 +3,9 @@ package io.reign.coord;
 import static org.junit.Assert.assertTrue;
 import io.reign.MasterTestSuite;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,218 +14,180 @@ import org.junit.Test;
 
 public class ZkLockTest {
 
-    private CoordinationService coordinationService;
+	private CoordinationService coordinationService;
+	private ThreadPoolExecutor executorService;
 
-    @Before
-    public void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
+		coordinationService = MasterTestSuite.getReign().getService("coord");
+		executorService = new ThreadPoolExecutor(5, 10, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 
-        coordinationService = MasterTestSuite.getReign().getService("coord");
+	}
 
-    }
+	@Test
+	public void testTryLock() throws Exception {
+		final AtomicInteger acquiredCount = new AtomicInteger(0);
 
-    @Test
-    public void testTryLock() throws Exception {
-        final AtomicInteger acquiredCount = new AtomicInteger(0);
+		Runnable t1 = new Runnable() {
+			@Override
+			public void run() {
+				DistributedLock lock = coordinationService.getLock("reign", "testTryLock");
+				try {
+					if (lock.tryLock()) {
+						acquiredCount.incrementAndGet();
+						Thread.sleep(10000);
+					}
+				} catch (Exception e1) {
+				} finally {
+					lock.unlock();
+					lock.destroy();
+				}
+			}
+		};
 
-        Thread t1 = new Thread() {
-            @Override
-            public void run() {
-                DistributedLock lock = coordinationService.getLock("reign", "testTryLock");
-                try {
-                    if (lock.tryLock()) {
-                        acquiredCount.incrementAndGet();
-                    }
-                } finally {
-                    lock.unlock();
-                    lock.destroy();
-                }
-                synchronized (this) {
-                    this.notifyAll();
-                }
-            }
-        };
+		Runnable t2 = new Runnable() {
+			@Override
+			public void run() {
+				DistributedLock lock = coordinationService.getLock("reign", "testTryLock");
+				try {
+					if (lock.tryLock()) {
+						acquiredCount.incrementAndGet();
+					}
+				} catch (Exception e1) {
+				} finally {
+					lock.unlock();
+					lock.destroy();
+				}
+			}
+		};
 
-        Thread t2 = new Thread() {
-            @Override
-            public void run() {
-                DistributedLock lock = coordinationService.getLock("reign", "testTryLock");
-                try {
-                    try {
-                        if (lock.tryLock()) {
-                            acquiredCount.incrementAndGet();
-                            throw new RuntimeException();
-                        }
-                    } finally {
-                        lock.unlock();
-                        lock.destroy();
-                    }
-                } catch (Exception e) {
+		Runnable t3 = new Runnable() {
+			@Override
+			public void run() {
+				DistributedLock lock = coordinationService.getLock("reign", "testTryLock");
+				try {
+					if (lock.tryLock()) {
+						acquiredCount.incrementAndGet();
+					}
+				} catch (Exception e1) {
+				} finally {
+					lock.unlock();
+					lock.destroy();
+				}
+			}
+		};
+		Runnable t4 = new Runnable() {
+			@Override
+			public void run() {
+				DistributedLock lock = coordinationService.getLock("reign", "testTryLock");
+				try {
+					if (lock.tryLock()) {
+						acquiredCount.incrementAndGet();
+					}
+				} catch (Exception e1) {
+				} finally {
+					lock.unlock();
+					lock.destroy();
+				}
+			}
+		};
 
-                }
-                synchronized (this) {
-                    this.notifyAll();
-                }
-            }
-        };
+		executorService.submit(t1);
+		executorService.submit(t2);
+		executorService.submit(t3);
+		executorService.submit(t4);
+		while (executorService.getCompletedTaskCount() < 4) {
+			Thread.sleep(1000);
+		}
+		assertTrue(acquiredCount.get() == 1);
+	}
 
-        Thread t3 = new Thread() {
-            @Override
-            public void run() {
-                DistributedLock lock = coordinationService.getLock("reign", "testTryLock");
-                try {
-                    try {
-                        if (lock.tryLock()) {
-                            acquiredCount.incrementAndGet();
-                            Thread.sleep(1000);
-                            throw new RuntimeException();
-                        }
-                    } finally {
-                        lock.unlock();
-                        lock.destroy();
-                    }
-                } catch (Exception e) {
+	@Test
+	public void testDistributedLock() throws Exception {
 
-                }
-                synchronized (this) {
-                    this.notifyAll();
-                }
-            }
-        };
-        Thread t4 = new Thread() {
-            @Override
-            public void run() {
-                DistributedLock lock = coordinationService.getLock("reign", "testTryLock");
-                try {
-                    if (lock.tryLock()) {
-                        acquiredCount.incrementAndGet();
-                    }
-                } finally {
-                    lock.unlock();
-                    lock.destroy();
-                }
-                synchronized (this) {
-                    this.notifyAll();
-                }
-            }
-        };
+		// use StringBuffer because it is synchronized and thread-safe
+		final AtomicInteger count = new AtomicInteger(0);
 
-        t1.start();
-        synchronized (t1) {
-            t1.wait(5000);
-        }
-        t2.start();
-        synchronized (t2) {
-            t2.wait(5000);
-        }
-        t3.start();
-        t4.start();
-        synchronized (t4) {
-            t4.wait(5000);
-        }
+		// threads to simulate multiple processes
+		final Runnable t1 = new Runnable() {
+			@Override
+			public void run() {
+				DistributedLock lock = coordinationService.getLock("clusterA", "test-lock-1");
+				lock.lock();
+				try {
+					int current = count.get();
+					Thread.sleep((int) (10000 * Math.random()) + 15000);
+					count.set(current + 1);
+				} catch (Exception e) {
+				} finally {
+					lock.unlock();
+					lock.destroy();
+				}
+			}
+		};
+		Runnable t2 = new Runnable() {
+			@Override
+			public void run() {
+				DistributedLock lock = coordinationService.getLock("clusterA", "test-lock-1");
+				lock.lock();
+				try {
+					int current = count.get();
+					Thread.sleep((int) (10000 * Math.random()));
+					count.set(current + 1);
+				} catch (Exception e) {
+				} finally {
+					lock.unlock();
+					lock.destroy();
+				}
+			}
+		};
+		Runnable t3 = new Runnable() {
+			@Override
+			public void run() {
+				DistributedLock lock = coordinationService.getLock("clusterA", "test-lock-1");
+				lock.lock();
+				try {
+					int current = count.get();
+					Thread.sleep((int) (10000 * Math.random()));
+					count.set(current + 1);
+				} catch (Exception e) {
+				} finally {
+					lock.unlock();
+					lock.destroy();
+				}
+			}
+		};
+		final AtomicBoolean t4DidNotAcquire = new AtomicBoolean(false);
+		Runnable t4 = new Runnable() {
+			@Override
+			public void run() {
+				DistributedLock lock = coordinationService.getLock("clusterA", "test-lock-1");
+				if (!lock.tryLock()) {
+					t4DidNotAcquire.set(true);
+					return;
+				}
+				try {
+					int current = count.get();
+					Thread.sleep((int) (10000 * Math.random()));
+					count.set(current + 1);
+				} catch (Exception e) {
+				} finally {
+					lock.unlock();
+					lock.destroy();
+				}
+			}
+		};
 
-        assertTrue(acquiredCount.get() == 3);
-    }
+		executorService.submit(t1);
+		executorService.submit(t2);
+		executorService.submit(t3);
+		executorService.submit(t4);
+		while (executorService.getCompletedTaskCount() < 4) {
+			Thread.sleep(1000);
+		}
 
-    @Test
-    public void testDistributedLock() throws Exception {
+		assertTrue(t4DidNotAcquire.get());
+		assertTrue("Unexpected value:  " + count.get(), count.get() == 3);
 
-        // use StringBuffer because it is synchronized and thread-safe
-        final StringBuffer sb = new StringBuffer();
-
-        // threads to simulate multiple processes
-        Thread t1 = new Thread() {
-            @Override
-            public void run() {
-                DistributedLock lock = coordinationService.getLock("clusterA", "test-lock-1");
-                lock.lock();
-                try {
-                    synchronized (this) {
-                        this.notifyAll();
-                    }
-                    sb.append("1");
-                } catch (Exception e) {
-                } finally {
-                    lock.unlock();
-                    lock.destroy();
-                }
-            }
-        };
-        Thread t2 = new Thread() {
-            @Override
-            public void run() {
-                DistributedLock lock = coordinationService.getLock("clusterA", "test-lock-1");
-                lock.lock();
-                try {
-                    synchronized (this) {
-                        this.notifyAll();
-                    }
-                    sb.append("2");
-                } catch (Exception e) {
-                } finally {
-                    lock.unlock();
-                    lock.destroy();
-                }
-            }
-        };
-        Thread t3 = new Thread() {
-            @Override
-            public void run() {
-                DistributedLock lock = coordinationService.getLock("clusterA", "test-lock-1");
-                lock.lock();
-                try {
-                    synchronized (this) {
-                        this.notifyAll();
-                    }
-                    Thread.sleep(1000);
-                    sb.append("3");
-                    synchronized (this) {
-                        this.notifyAll();
-                    }
-                } catch (Exception e) {
-                } finally {
-                    lock.unlock();
-                    lock.destroy();
-                }
-            }
-        };
-        final AtomicBoolean t4DidNotAcquire = new AtomicBoolean(false);
-        Thread t4 = new Thread() {
-            @Override
-            public void run() {
-                DistributedLock lock = coordinationService.getLock("clusterA", "test-lock-1");
-                try {
-                    if (lock.tryLock()) {
-                        sb.append("4");
-                    } else {
-                        t4DidNotAcquire.set(true);
-                    }
-                } catch (Exception e) {
-                } finally {
-                    lock.unlock();
-                    lock.destroy();
-                }
-            }
-        };
-
-        t1.start();
-        synchronized (t1) {
-            t1.wait();
-        }
-        t2.start();
-        synchronized (t2) {
-            t2.wait();
-        }
-        t3.start();
-        synchronized (t3) {
-            t3.wait();
-        }
-        t4.start();
-        synchronized (t3) {
-            t3.wait();
-        }
-
-        assertTrue(t4DidNotAcquire.get());
-        assertTrue("Unexpected value:  " + sb, "123".equals(sb.toString()));
-
-    }
+	}
 }
