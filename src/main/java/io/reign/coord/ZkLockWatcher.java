8fec8f546e6ac1be55671a74491328845baf0556
Yen Pai <ypai@kompany.org>
2014-07-15 10:39:32 -0700

    removed extraneous logging

--- src/main/java/io/reign/coord/ZkLockWatcher.java
+++ src/main/java/io/reign/coord/ZkLockWatcher.java
@@ -31,118 +31,114 @@ import org.slf4j.LoggerFactory;
  * 
  */
 public class ZkLockWatcher implements Watcher {
-    private static final Logger logger = LoggerFactory.getLogger(ZkLockWatcher.class);
+	private static final Logger logger = LoggerFactory.getLogger(ZkLockWatcher.class);
 
-    private static AtomicInteger instancesOutstanding = new AtomicInteger(0);
+	private static AtomicInteger instancesOutstanding = new AtomicInteger(0);
 
-    private final String lockPath;
+	private final String lockPath;
 
-    private final String lockReservationPath;
+	private final String lockReservationPath;
 
-    public static long instancesOutstanding() {
-        return instancesOutstanding.get();
-    }
+	public static long instancesOutstanding() {
+		return instancesOutstanding.get();
+	}
 
-    public ZkLockWatcher(String lockPath, String lockReservationPath) {
-        instancesOutstanding.incrementAndGet();
+	public ZkLockWatcher(String lockPath, String lockReservationPath) {
+		instancesOutstanding.incrementAndGet();
 
-        this.lockPath = lockPath;
-        this.lockReservationPath = lockReservationPath;
+		this.lockPath = lockPath;
+		this.lockReservationPath = lockReservationPath;
 
-        if (logger.isDebugEnabled()) {
-            logger.debug("Created:  instancesOutstanding={}; lockName={}; lockReservation={}", new Object[] {
-                    instancesOutstanding.get(), lockPath, lockReservationPath });
-        }
-    }
+		if (logger.isDebugEnabled()) {
+			logger.debug("Created:  instancesOutstanding={}; lockName={}; lockReservation={}", new Object[] {
+			        instancesOutstanding.get(), lockPath, lockReservationPath });
+		}
+	}
 
-    public void destroy() {
-        // notify all waiters: shouldn't be any at this point
-        synchronized (this) {
-            this.notifyAll();
-        }
+	public void destroy() {
+		// notify all waiters: shouldn't be any at this point
+		synchronized (this) {
+			this.notifyAll();
+		}
 
-        if (logger.isDebugEnabled()) {
-            logger.debug("Destroyed:  instancesOutstanding={}; lockName={}; lockReservation={}", new Object[] {
-                    instancesOutstanding.get(), lockPath, lockReservationPath });
-        }
-        instancesOutstanding.decrementAndGet();
-    }
+		instancesOutstanding.decrementAndGet();
+		if (logger.isDebugEnabled()) {
+			logger.debug("Destroyed:  instancesOutstanding={}; lockName={}; lockReservation={}", new Object[] {
+			        instancesOutstanding.get(), lockPath, lockReservationPath });
+		}
+	}
 
-    public void waitForEvent(long waitTimeoutMs) throws InterruptedException {
-        if (waitTimeoutMs == 0) {
-            return;
-        }
+	public void waitForEvent(long waitTimeoutMs) throws InterruptedException {
+		if (waitTimeoutMs == 0) {
+			return;
+		}
 
-        synchronized (this) {
-            if (waitTimeoutMs == -1) {
-                long startTimestamp = System.currentTimeMillis();
+		logger.debug("waitForEvent():  instancesOutstanding={}; lockName={}; lockReservation={}", new Object[] {
+		        instancesOutstanding.get(), lockPath, lockReservationPath });
 
-                this.wait();
+		synchronized (this) {
+			if (waitTimeoutMs == -1) {
+				this.wait();
 
-                if (System.currentTimeMillis() - startTimestamp > 120000) {
-                    logger.warn("Waited over 120 seconds for lock event:  lockName={}; lockReservation={}",
-                            new Object[] { instancesOutstanding.get(), lockPath, lockReservationPath });
-                }
+			} else {
+				this.wait(waitTimeoutMs);
+			}
+		}
+	}
 
-            } else {
-                this.wait(waitTimeoutMs);
-            }
-        }
-    }
+	@Override
+	public void process(WatchedEvent event) {
+		// log if DEBUG
+		if (logger.isDebugEnabled()) {
+			logger.debug("***** Received ZooKeeper Event:  {}",
+			        ReflectionToStringBuilder.toString(event, ToStringStyle.DEFAULT_STYLE));
 
-    @Override
-    public void process(WatchedEvent event) {
-        // log if DEBUG
-        if (logger.isDebugEnabled()) {
-            logger.debug("***** Received ZooKeeper Event:  {}",
-                    ReflectionToStringBuilder.toString(event, ToStringStyle.DEFAULT_STYLE));
+		}
 
-        }
+		// process events
+		switch (event.getType()) {
+		case NodeCreated:
+		case NodeChildrenChanged:
+		case NodeDataChanged:
+		case NodeDeleted:
+			synchronized (this) {
+				this.notifyAll();
 
-        // process events
-        switch (event.getType()) {
-        case NodeCreated:
-        case NodeChildrenChanged:
-        case NodeDataChanged:
-        case NodeDeleted:
-            synchronized (this) {
-                this.notifyAll();
+				if (logger.isDebugEnabled()) {
+					logger.debug("Notifying threads waiting on LockWatcher:  lockWatcher.hashcode()=" + this.hashCode()
+					        + "; instancesOutstanding=" + instancesOutstanding.get() + "; lockName=" + lockPath
+					        + "; lockReservation=" + lockReservationPath);
+				}
+			}
+			break;
 
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Notifying threads waiting on LockWatcher:  lockWatcher.hashcode()=" + this.hashCode()
-                            + "; instancesOutstanding=" + instancesOutstanding.get() + "; lockName=" + lockPath
-                            + "; lockReservation=" + lockReservationPath);
-                }
-            }
-            break;
+		case None:
+			Event.KeeperState eventState = event.getState();
+			if (eventState == Event.KeeperState.SyncConnected) {
+				// connection event: check children
+				synchronized (this) {
+					this.notifyAll();
+				}
 
-        case None:
-            Event.KeeperState eventState = event.getState();
-            if (eventState == Event.KeeperState.SyncConnected) {
-                // connection event: check children
-                synchronized (this) {
-                    this.notifyAll();
-                }
+			} else if (eventState == Event.KeeperState.Disconnected || eventState == Event.KeeperState.Expired) {
+				// disconnected: notifyAll so we can check children again on
+				// reconnection
+				synchronized (this) {
+					this.notifyAll();
+				}
 
-            } else if (eventState == Event.KeeperState.Disconnected || eventState == Event.KeeperState.Expired) {
-                // disconnected: notifyAll so we can check children again on
-                // reconnection
-                synchronized (this) {
-                    this.notifyAll();
-                }
+			} else {
+				logger.warn("Unhandled event state:  "
+				        + ReflectionToStringBuilder.toString(event, ToStringStyle.DEFAULT_STYLE));
+			}
+			break;
 
-            } else {
-                logger.warn("Unhandled event state:  "
-                        + ReflectionToStringBuilder.toString(event, ToStringStyle.DEFAULT_STYLE));
-            }
-            break;
+		default:
+			logger.warn("Unhandled event type:  "
+			        + ReflectionToStringBuilder.toString(event, ToStringStyle.DEFAULT_STYLE));
+		}
 
-        default:
-            logger.warn("Unhandled event type:  "
-                    + ReflectionToStringBuilder.toString(event, ToStringStyle.DEFAULT_STYLE));
-        }
+		// }// if
 
-        // }// if
-
-    }// process()
+	}// process()
 }// class
