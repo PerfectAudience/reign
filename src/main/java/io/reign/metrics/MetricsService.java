6310e69c1b31999f9e1effc69294bc42544ddf1f
ypai <ypai@kompany.org>
2014-09-25 22:49:18 -0700

    updated copyright message

--- src/main/java/io/reign/metrics/MetricsService.java
+++ src/main/java/io/reign/metrics/MetricsService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Yen Pai ypai@reign.io
+ * Copyright 2014 Yen Pai ypai@reign.io
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
